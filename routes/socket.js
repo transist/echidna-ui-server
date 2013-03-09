a70bae9877ba9d675136c2dfb6a842a0dc743995
clemsos <clement.renaud@gmail.com>
2013-03-09 11:30:48 +0800

    delete socket init for loop

--- routes/socket.js
+++ routes/socket.js
@@ -7,10 +7,11 @@ var dataGenerator = require('../echidna-data/faker.js')
 var dataParse     = require('../echidna-data/d3container.js')
 
 
+var count = 5;
+
 module.exports = function (socket) {
 
     
-    var count = 5;
     // Send init slices
     socket.on('feedconfig', function (data){
         
@@ -21,23 +22,29 @@ module.exports = function (socket) {
 
     })
 
-    for (var i = 0; i < 50; i++) {
+    // for (var i = 0; i < 50; i++) {
 
-        socket.emit('slice', {
+    //     socket.emit('slice', {
 
-          datapoint : dataGenerator.newSlice(count ,1,true)
+    //       datapoint : dataGenerator.newSlice(count, i)
     
-        });
+    //     });
         
-    };
+    // };
+
+    // var z = 0;
 
     setInterval(function () {
+        
+        // if(z % 10 == 0) count = count+1;
 
         socket.emit('slice', {
 
-          datapoint : dataGenerator.newSlice(count ,1,false)
+          datapoint : dataGenerator.newSlice(count, 0)
     
         });
+
+        // z++;
 
       }, 1000);
       
